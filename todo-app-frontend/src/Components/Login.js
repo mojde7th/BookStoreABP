1ce20a5370b8dd1ddf2e705da749867f14c78bcd
mojde7th <M4454m4454@>
2024-07-21 10:53:02 +0330

    With Reload to Specific Page

--- todo-app-frontend/src/Components/Login.js
+++ todo-app-frontend/src/Components/Login.js
@@ -1,19 +1,20 @@
 // src/components/Login.js
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { login } from '../services/authService';
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/books';
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const response = await login(username, password);
             localStorage.setItem('token', response.data.token);
-            navigate('/books');
+            navigate(from, { replace: true });
         } catch (error) {
             console.error('Login failed', error);
             alert('Login failed');
